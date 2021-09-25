3f987af6005f6129fe595b6984a92b2e079da362
GenSven <shensven2016@gmail.com>
2021-09-25 15:19:12 +0800

    chore: install React Navigation & their dependencies

---
+++ src/screens/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Home: React.FC = () => {
+  return (
+    <View>
+      <Text>Home Test</Text>
+    </View>
+  );
+};
+
+export default Home;
