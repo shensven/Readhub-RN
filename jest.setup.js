eaa121de969312d4bf5ca5522502cbf00f8d6f17
GenSven <shensven2016@gmail.com>
2021-09-26 01:07:37 +0800

    chore: global.__reanimatedWorkletInit for test environment fixes

--- jest.setup.js
+++ jest.setup.js
@@ -2,4 +2,16 @@ import {jest} from '@jest/globals';
 import 'react-native-gesture-handler/jestSetup';
 import {View as mockView} from 'react-native';
 
-jest.mock('react-native-tab-view', () => ({TabView: mockView}));
+global.__reanimatedWorkletInit = jest.fn();
+
+jest.mock('react-native-reanimated', () => {
+  const Reanimated = require('react-native-reanimated/mock');
+  //   Reanimated.default.call = () => {};
+  return Reanimated;
+});
+
+jest.mock('react-native-tab-view', () => ({
+  TabView: mockView,
+}));
+
+jest.useFakeTimers();
