3dd187856932203c726c67373bc48312ffe58112
GenSven <shensven2016@gmail.com>
2021-09-25 17:35:40 +0800

    chore: install @react-navigation/material-top-tabs & deps

--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,5 @@
-// import {jest} from '@jest/globals';
+import {jest} from '@jest/globals';
 import 'react-native-gesture-handler/jestSetup';
+import {View as mockView} from 'react-native';
+
+jest.mock('react-native-tab-view', () => ({TabView: mockView}));
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,42 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {ScrollView, Text, View} from 'react-native';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+
+const Tab = createMaterialTopTabNavigator();
+
+const Topics: React.FC = () => {
+  return (
+    <View style={{flex: 1}}>
+      <ScrollView>
+        <Text>T</Text>
+      </ScrollView>
+    </View>
+  );
+};
+
+const News: React.FC = () => {
+  return (
+    <View>
+      <Text>N</Text>
+    </View>
+  );
+};
+
+const Tech: React.FC = () => {
+  return (
+    <View>
+      <Text>N</Text>
+    </View>
+  );
+};
 
 const Home: React.FC = () => {
   return (
-    <View>
-      <Text>Home Test</Text>
-    </View>
+    <Tab.Navigator>
+      <Tab.Screen name="Topics" component={Topics} />
+      <Tab.Screen name="News" component={News} />
+      <Tab.Screen name="Tech" component={Tech} />
+    </Tab.Navigator>
   );
 };
 
