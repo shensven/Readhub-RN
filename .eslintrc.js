126975dfec0a0126dddcd9baa7f7b45ecaba9df5
GenSven <shensven2016@gmail.com>
2021-09-28 22:05:55 +0800

    chore: minor ESLint rules update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'react-native/no-unused-styles': 1,
+  },
 };
