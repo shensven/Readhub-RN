91388cb855be592257e547af70662e6f35f6bfb0
GenSven <shensven2016@gmail.com>
2021-10-01 15:20:08 +0800

    chore: add axios interceptor

---
+++ src/utils/appAxios.ts
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import host from './host';
+
+const appAxios = axios.create({
+  baseURL: host.url,
+  timeout: 5000,
+});
+
+appAxios.interceptors.request.use(
+  config => {
+    return config;
+  },
+  error => {
+    console.log(error);
+    return Promise.reject(error);
+  },
+);
+
+appAxios.interceptors.response.use(
+  config => {
+    return config;
+  },
+  error => {
+    console.log(error);
+    return Promise.reject(error);
+  },
+);
+
+export default appAxios;
---
+++ src/utils/host.ts
@@ -0,0 +1,3 @@
+export default {
+  url: 'https://api.readhub.cn/',
+};
