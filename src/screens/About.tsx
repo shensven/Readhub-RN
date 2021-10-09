3c35adfc08c2f4e435714f427c8993f44cd96258
GenSven <shensven2016@gmail.com>
2021-10-09 22:37:48 +0800

    fix: minor UI update

--- src/screens/About.tsx
+++ src/screens/About.tsx
@@ -1,8 +1,11 @@
 import React, {useLayoutEffect} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image, StyleSheet} from 'react-native';
 import {useNavigation, useRoute} from '@react-navigation/native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 const About: React.FC = () => {
+  const insets = useSafeAreaInsets();
+
   const navigation = useNavigation();
   const route = useRoute();
 
@@ -16,10 +19,44 @@ const About: React.FC = () => {
   }, [navigation, route]);
 
   return (
-    <View>
-      <Text>about</Text>
+    <View style={styles.root}>
+      <View style={styles.top}>
+        <Image source={require('../assets/AppIcon/AppIcon120.png')} style={styles.app_logo} />
+        <Text style={styles.app_name}>ReadHubn</Text>
+        <Text style={styles.app_ver}>Version 1.0 (1)</Text>
+      </View>
+      <Text style={[styles.description, {marginBottom: insets.bottom + 24}]}>Made with ❤️ in Kunming by GenSven</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  top: {
+    alignItems: 'center',
+  },
+  app_logo: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    marginTop: 48,
+  },
+  app_name: {
+    marginTop: 16,
+  },
+  app_ver: {
+    fontSize: 10,
+    fontWeight: 'bold',
+    marginTop: 4,
+  },
+  description: {
+    fontSize: 10,
+    fontWeight: 'bold',
+  },
+});
 
 export default About;
