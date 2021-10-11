36032ba0dab8f15d9677a8f1e59f72b3cbd5465f
GenSven <shensven2016@gmail.com>
2021-10-11 21:58:21 +0800

    fix: render final view after axios get

--- src/screens/DetailTopic.tsx
+++ src/screens/DetailTopic.tsx
@@ -105,12 +105,14 @@ const DetailTopic: React.FC = () => {
   const route = useRoute<ScreenRouteProp>();
   const {id} = route.params;
 
+  const [hasFinalView, setHasFinalView] = useState<boolean>(false);
   const [detail, setDetail] = useState<Detail>({} as Detail);
 
   const getTopicSummary = async () => {
     const resp: {data: Detail} = await appAxios.get(`/topic/${route.params.id}`);
     // console.log('getTopicSummary', resp.data);
     setDetail(resp.data);
+    setHasFinalView(true);
   };
 
   useLayoutEffect(() => {
@@ -126,62 +128,69 @@ const DetailTopic: React.FC = () => {
     });
   }, [navigation, route]);
 
-  return (
-    <ScrollView contentContainerStyle={styles.root}>
-      <Text style={styles.title}>{detail.title}</Text>
-      <Text style={styles.publishDate}>{dayjs(detail.publishDate).fromNow()}</Text>
-      <Text selectable={true} style={styles.summary}>
-        {detail.summary}
-      </Text>
-      <View style={styles.mid}>
-        {detail.hasInstantView ? (
-          <TouchableRipple
-            borderless={true}
-            rippleColor="rgba(46,117,213,0.8)"
-            style={styles.instant}
-            onPress={() => navigation.navigate('Instant', {id})}>
-            <>
-              <Ionicons name="glasses-outline" size={24} color="rgb(46,117,213)" />
-              <Text style={styles.instant_label}>即时预览</Text>
-            </>
-          </TouchableRipple>
-        ) : (
-          <View />
-        )}
-        <IconButton icon="share-variant" size={14} color="#FFFFFF" style={styles.iconbtn} onPress={() => {}} />
-      </View>
-      <View style={styles.bottom}>
-        <View style={styles.bottom_title}>
-          <Ionicons name="newspaper-outline" size={16} />
-          <Text style={styles.bottom_title_right}>媒体报道</Text>
-        </View>
-        {detail?.newsArray?.map((newsReporterItem: NewsArray, newsReporterIndex: number) => (
-          <View key={newsReporterIndex} style={styles.bottom_item}>
-            <Text>{'- '}</Text>
-            <View>
-              <Text numberOfLines={1}>{newsReporterItem.title}</Text>
-              <Text style={styles.bottom_subTitle}>{newsReporterItem.siteName}</Text>
+  switch (hasFinalView) {
+    case true:
+      return (
+        hasFinalView && (
+          <ScrollView contentContainerStyle={styles.root}>
+            <Text style={styles.title}>{detail.title}</Text>
+            <Text style={styles.publishDate}>{dayjs(detail.publishDate).fromNow()}</Text>
+            <Text selectable={true} style={styles.summary}>
+              {detail.summary}
+            </Text>
+            <View style={styles.mid}>
+              {detail.hasInstantView ? (
+                <TouchableRipple
+                  borderless={true}
+                  rippleColor="rgba(46,117,213,0.8)"
+                  style={styles.instant}
+                  onPress={() => navigation.navigate('Instant', {id})}>
+                  <>
+                    <Ionicons name="glasses-outline" size={24} color="rgb(46,117,213)" />
+                    <Text style={styles.instant_label}>即时预览</Text>
+                  </>
+                </TouchableRipple>
+              ) : (
+                <View />
+              )}
+              <IconButton icon="share-variant" size={14} color="#FFFFFF" style={styles.iconbtn} onPress={() => {}} />
             </View>
-          </View>
-        ))}
-      </View>
-      <View style={styles.bottom}>
-        <View style={styles.bottom_title}>
-          <Ionicons name="time-outline" size={17} />
-          <Text style={styles.bottom_title_right}>事件追踪</Text>
-        </View>
-        {detail?.timeline?.topics?.map((topicsItem: Topics, topicIndex: number) => (
-          <View key={topicIndex} style={styles.bottom_item}>
-            <Text>{'- '}</Text>
-            <View>
-              <Text>{topicsItem.title}</Text>
-              <Text style={styles.bottom_subTitle}>{dayjs(topicsItem.createdAt).format('YYYY-MM-DD')}</Text>
+            <View style={styles.bottom}>
+              <View style={styles.bottom_title}>
+                <Ionicons name="newspaper-outline" size={16} />
+                <Text style={styles.bottom_title_right}>媒体报道</Text>
+              </View>
+              {detail?.newsArray?.map((newsReporterItem: NewsArray, newsReporterIndex: number) => (
+                <View key={newsReporterIndex} style={styles.bottom_item}>
+                  <Text>{'- '}</Text>
+                  <View>
+                    <Text numberOfLines={1}>{newsReporterItem.title}</Text>
+                    <Text style={styles.bottom_subTitle}>{newsReporterItem.siteName}</Text>
+                  </View>
+                </View>
+              ))}
             </View>
-          </View>
-        ))}
-      </View>
-    </ScrollView>
-  );
+            <View style={styles.bottom}>
+              <View style={styles.bottom_title}>
+                <Ionicons name="time-outline" size={17} />
+                <Text style={styles.bottom_title_right}>事件追踪</Text>
+              </View>
+              {detail?.timeline?.topics?.map((topicsItem: Topics, topicIndex: number) => (
+                <View key={topicIndex} style={styles.bottom_item}>
+                  <Text>{'- '}</Text>
+                  <View>
+                    <Text>{topicsItem.title}</Text>
+                    <Text style={styles.bottom_subTitle}>{dayjs(topicsItem.createdAt).format('YYYY-MM-DD')}</Text>
+                  </View>
+                </View>
+              ))}
+            </View>
+          </ScrollView>
+        )
+      );
+    default:
+      return null;
+  }
 };
 
 const styles = StyleSheet.create({
