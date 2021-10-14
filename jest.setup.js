73223e4959c4d2bca7dfaf2a81e322adbb0e556e
GenSven <shensven2016@gmail.com>
2021-10-14 23:25:51 +0800

    fix: setting react-native-device-info with jest

--- jest.setup.js
+++ jest.setup.js
@@ -1,11 +1,14 @@
 import {jest} from '@jest/globals';
+import mockRNDeviceInfo from 'react-native-device-info/jest/react-native-device-info-mock';
 import 'react-native-gesture-handler/jestSetup';
+
 // import {View as mockView} from 'react-native';
 
 jest.useFakeTimers();
 
 global.__reanimatedWorkletInit = jest.fn();
 
+jest.mock('react-native-device-info', () => mockRNDeviceInfo);
 jest.mock('react-native-reanimated', () => {
   const Reanimated = require('react-native-reanimated/mock');
   Reanimated.default.call = () => {};
