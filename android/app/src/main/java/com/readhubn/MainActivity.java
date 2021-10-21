015407f27d85bf28e3262def17c739deeec8bfe1
GenSven <shensven2016@gmail.com>
2021-10-21 10:26:24 +0800

    chore:  set up react-native-gesture-handler properly for Android

--- android/app/src/main/java/com/readhubn/MainActivity.java
+++ android/app/src/main/java/com/readhubn/MainActivity.java
@@ -9,6 +9,9 @@ import android.view.Window;
 import android.view.WindowManager;
 import androidx.core.view.WindowCompat;
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 import com.zoontek.rnbootsplash.RNBootSplash;
 
 public class MainActivity extends ReactActivity {
@@ -43,6 +46,16 @@ public class MainActivity extends ReactActivity {
     // Layout edge-to-edge
     WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
   }
+
+   @Override
+   protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+      @Override
+      protected ReactRootView createRootView() {
+        return new RNGestureHandlerEnabledRootView(MainActivity.this);
+      }
+    };
+   }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
