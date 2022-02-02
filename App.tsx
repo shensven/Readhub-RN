a36c8562780ac818dd47db3b941171b8d8677047
SvenFE <shensven2016@gmail.com>
2022-02-02 16:26:08 +0800

    refactor: minor UI update

--- App.tsx
+++ App.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from './src/screens/Home';
+import HomeRight from './src/headers/HomeRight';
+
+const Stack = createNativeStackNavigator();
 
 const App: React.FC = () => {
-  return null;
+  return (
+    <NavigationContainer
+      onReady={() => console.log('NavigationContainer Ready')}>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Readhub Native"
+          component={Home}
+          options={{
+            headerTitleAlign: 'left',
+            headerRight: () => <HomeRight />,
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default App;
---
+++ src/headers/HomeRight.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const HomeRight: React.FC = () => {
+  return <Text>Right</Text>;
+};
+
+export default HomeRight;
