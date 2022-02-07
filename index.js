98bc444f8c2a203445139bcb35c2caf513fc7ea8
SvenFE <shensven2016@gmail.com>
2022-02-07 23:20:37 +0800

    refactor: add PaperProvider

--- index.js
+++ index.js
@@ -4,7 +4,17 @@
 
 import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
+import {Provider as PaperProvider} from 'react-native-paper';
 import App from './App';
 import {name as appName} from './app.json';
+import React from 'react';
 
-AppRegistry.registerComponent(appName, () => App);
+const Main = () => {
+  return (
+    <PaperProvider>
+      <App />
+    </PaperProvider>
+  );
+};
+
+AppRegistry.registerComponent(appName, () => Main);
