85e30a468908f2814392211ad77ca7aec59951b9
SvenFE <shensven2016@gmail.com>
2022-02-12 16:39:34 +0800

    chore: mock react-native-reanimated

--- jest.setup.js
+++ jest.setup.js
@@ -3,6 +3,14 @@ import 'react-native-gesture-handler/jestSetup';
 
 jest.useFakeTimers();
 
+global.__reanimatedWorkletInit = () => {};
+
+jest.mock('react-native-reanimated', () => {
+  const Reanimated = require('react-native-reanimated/mock');
+  Reanimated.default.call = () => {};
+  return Reanimated;
+});
+
 jest.doMock('react-native-bootsplash', () => {
   return {
     hide: jest.fn().mockResolvedValueOnce(),
