0fc9a3f5aabc11e6e958208bbb27728f147f4c9e
SvenFE <shensven2016@gmail.com>
2022-09-20 15:09:28 +0800

    fix: currentColor does not work as expected

--- src/assets/icons/IcRoundInfo.tsx
+++ src/assets/icons/IcRoundInfo.tsx
@@ -11,7 +11,7 @@ const IcRoundInfo: React.FC<Props> = props => {
   return (
     <Svg width={size} height={size} color={color} viewBox="0 0 24 24" {...props}>
       <Path
-        fill="currentColor"
+        fill={color}
         d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2zm0 15c-.55 0-1-.45-1-1v-4c0-.55.45-1 1-1s1 .45 1 1v4c0 .55-.45 1-1 1zm1-8h-2V7h2v2z"
       />
     </Svg>
