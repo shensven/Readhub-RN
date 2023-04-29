75855ffeea35f4cabb7dd921c6dc41e7eba4e1a7
SvenFE <shensven2016@gmail.com>
2023-04-29 23:34:58 +0800

    feat: add persistence to appearance

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import 'react-native-gesture-handler';
 import {Provider as PaperProvider} from 'react-native-paper';
 import {StatusBar} from 'react-native-bars';
 import {useAppearance} from '@/utils/appearance';
+import {useAsyncStorageFlipperPlugin} from '@/utils/asyncStorage';
 import AppStack from './src/AppStack';
-import 'react-native-gesture-handler';
 
 function App() {
+  useAsyncStorageFlipperPlugin();
   const {statusBarStyle, paperTheme} = useAppearance();
 
   return (
--- src/utils/status/atom.ts
+++ src/utils/status/atom.ts
@@ -1,10 +1,14 @@
 import {atom} from 'jotai';
+import {atomWithStorage, createJSONStorage} from 'jotai/utils';
 import type {SystemBarStyle} from 'react-native-bars';
 import type {MD3Theme} from 'react-native-paper';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import {navigationLightTheme, paperLightTheme} from '../appearance';
 import type {NavigationTheme} from '../appearance';
 
-const themeSchemeAtom = atom<'light' | 'dark' | 'system'>('system');
+const storage: any = createJSONStorage(() => AsyncStorage);
+
+const themeSchemeAtom = atomWithStorage<'light' | 'dark' | 'system'>('themeScheme', 'system', storage);
 const statusBarStyleAtom = atom<SystemBarStyle>('dark-content');
 const paperThemeAtom = atom<MD3Theme>(paperLightTheme);
 const navigationThemeAtom = atom<NavigationTheme>(navigationLightTheme);
